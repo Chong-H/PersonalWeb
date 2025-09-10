697d18ef2ec2ca3e318e1e589144b25828c13f7b
PC <posedgeclock>
2025-09-10 16:06:54 +0800

    适应cloudflare部署

--- vite.config.ts
+++ vite.config.ts
@@ -5,16 +5,34 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue(), vueDevTools()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+// export default defineConfig({
+//   plugins: [vue(), vueDevTools()],
+//   resolve: {
+//     alias: {
+//       '@': fileURLToPath(new URL('./src', import.meta.url)),
+//     },
+//   },
+//   base: '/PersonalWeb/',
+//   server: {
+//     host: '0.0.0.0',
+//     port: 5173, // 可自定义端口
+//   },
+// })
+
+export default defineConfig(() => {
+  const isCloudflare = process.env.CF_PAGES === '1' // ✅ Cloudflare Pages 环境会自动有这个变量
+
+  return {
+    plugins: [vue(), vueDevTools()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
     },
-  },
-  base: '/PersonalWeb/',
-  server: {
-    host: '0.0.0.0',
-    port: 5173, // 可自定义端口
-  },
+    base: isCloudflare ? '/' : '/PersonalWeb/', // 默认 GitHub Pages，用 Cloudflare 时改成 /
+    server: {
+      host: '0.0.0.0',
+      port: 5173,
+    },
+  }
 })
