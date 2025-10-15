d56e322fdb9efd76292ff9f22fbe7937607aec70
PC <posedgeclock>
2025-10-15 23:19:33 +0800

    change codes color display by highlight.js

--- src/components/display/CollapsibleCodeBlock.vue
+++ src/components/display/CollapsibleCodeBlock.vue
@@ -1,18 +1,23 @@
 <template>
   <div class="collapsible-code-block">
     <div class="code-preview" v-if="!expanded">
-      <pre class="code-block"><code>{{ preview }}</code></pre>
+      <pre
+        class="code-block"
+      ><code ref="previewCode" class="language-cpp">{{ preview }}</code></pre>
       <el-button type="primary" size="small" @click="expanded = true">展开全部代码</el-button>
     </div>
     <div v-else>
-      <pre class="code-block"><code>{{ code }}</code></pre>
+      <pre class="code-block"><code ref="fullCode" class="language-cpp">{{ code }}</code></pre>
       <el-button type="default" size="small" @click="expanded = false">收起</el-button>
     </div>
   </div>
 </template>
 
 <script setup>
-import { ref, computed } from 'vue'
+import hljs from 'highlight.js' // 引入highlight.js
+import 'highlight.js/styles/atom-one-dark.css' // 引入一个深色主题（可选其他主题）
+import { ref, computed, onMounted, watch, nextTick } from 'vue' // 添加所有需要的导入
+
 const props = defineProps({
   code: {
     type: String,
@@ -23,11 +28,37 @@ const props = defineProps({
     default: 8,
   },
 })
+
 const expanded = ref(false)
+// 定义两个 ref 指向代码块 DOM 元素
+const previewCode = ref(null)
+const fullCode = ref(null)
+
 const preview = computed(() => {
   const lines = props.code.split('\n')
   if (lines.length <= props.previewLines) return props.code
   return lines.slice(0, props.previewLines).join('\n') + '\n...'
+})
+
+// 组件挂载后初始化高亮
+onMounted(() => {
+  if (previewCode.value) {
+    hljs.highlightElement(previewCode.value) // 高亮预览代码
+  }
+})
+
+// 监听展开/收起状态变化,更新高亮
+watch(expanded, (newVal) => {
+  nextTick(() => {
+    // 用 nextTick 确保 DOM 已更新
+    if (newVal) {
+      // 展开时,高亮完整代码
+      if (fullCode.value) hljs.highlightElement(fullCode.value)
+    } else {
+      // 收起时,重新高亮预览代码(防止折叠后样式丢失)
+      if (previewCode.value) hljs.highlightElement(previewCode.value)
+    }
+  })
 })
 </script>
 
